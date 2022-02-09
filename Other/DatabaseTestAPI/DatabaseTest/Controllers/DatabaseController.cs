3de567713c46278b85d94e17354c72056729cdc4
TroliksMan <michal.stankoml@gmail.com>
2022-02-09 21:15:58 +0100

    DatabaseAPI

---
+++ Other/DatabaseTestAPI/DatabaseTest/Controllers/DatabaseController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DatabaseController : ControllerBase
+    {
+        MyContext myContext = new MyContext();
+
+        [HttpGet("Get1")]
+        public IEnumerable<Client> Get()
+        {
+            return myContext.Clients;
+        }
+        [HttpGet("Get2")]
+        public IEnumerable<Client> GetTest()
+        {
+            return myContext.Clients;
+        }
+
+        [HttpPost]
+        public void Post(string name, string ip, string mac)
+        {
+            Client client = new Client() { Name = name, IpAddress = ip, MacAddress = mac };
+            myContext.Clients.Add(client);
+
+            myContext.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            Client client = myContext.Clients.Find(id);
+            myContext.Clients.Remove(client);
+
+            myContext.SaveChanges();
+        }
+    }
+}
---
+++ Other/DatabaseTestAPI/DatabaseTest/Controllers/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTest.Controllers
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=3b2_hasekfrantisek_db1;user=hasekfrantisek;password=123456;SslMode=none");
+        }
+
+    }
+
+}
