ccd75743fa7f51c05b7a4aba0efd46e7b224048b
Zarovka22 <frantisek.hasek22@gmail.com>
2022-02-19 11:45:34 +0100

    struktura-web

--- Web/Pain-web/src/app/app.module.ts
+++ Web/Pain-web/src/app/app.module.ts
@@ -7,11 +7,27 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatRadioModule} from "@angular/material/radio";
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PageSkeletonComponent } from './page-skeleton/page-skeleton.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DashboardComponent } from '../Content/dashboard/dashboard.component';
+import { LogsComponent } from '../Content/logs/logs.component';
+import { ConfigsComponent } from '../Content/configs/configs.component';
+import { ClientsComponent } from '../Content/clients/clients.component';
+import { UsersComponent } from '../Content/users/users.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    PageSkeletonComponent,
+    NavBarComponent,
+    DashboardComponent,
+    LogsComponent,
+    ConfigsComponent,
+    ClientsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
