87ff49d4e72bc9286da4b97475b95348e9bb2ddb
TroliksMan <michal.stankoml@gmail.com>
2022-02-25 20:43:38 +0100

    Config models

--- Web/Pain-Web/src/app/models/config.model.ts
+++ Web/Pain-Web/src/app/models/config.model.ts
@@ -1,11 +1,16 @@
+interface Destination {
+  type: string;
+  destination: string;
+}
 export class Config {
   public name : string = '';
   public create_date : string = '';
   public backup_type : string = '';
+  public backup_format : string = '';
   public creator : string = '';
   public retention : string = '';
   public frequency : string = '';
   public PCs : string[] = [];
   public sources : string[] = [];
-  public destinations : string[] = [];
+  public destinations : Destination[] = [];
 }
