86b2c851fd39d662c62d3b575a2f3c6443bdaea8
TroliksMan <michal.stankoml@gmail.com>
2022-03-22 00:29:07 +0100

    DaemonApp-COMPLETužJO

--- Apps/DaemonOfPain/DaemonOfPain/Program.cs
+++ Apps/DaemonOfPain/DaemonOfPain/Program.cs
@@ -9,33 +9,6 @@ namespace DaemonOfPain
     {
         static void Main(string[] args)
         {
-            MetadataService md = new MetadataService();
-            //List<MetadataItem> items = new List<MetadataItem>();
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    MetadataItem item = new MetadataItem("C:\\sdaad", _itemChange.ADDED);
-            //    items.Add(item);
-            //}
-            //Metadata metadata = new Metadata(1, "gg", "", DateTime.Now, _BackupType.FB, new int[2] { 1, 1 }) { Items = items };
-
-            //md.WriteMetadata(@"C:\Users\František\Desktop", metadata);
-            //Console.ReadKey();
-
-            //List<Metadata> m = md.MetaSearcher(@"C:\Users\František\Desktop\BackupTesting\SourceDir");
-            //Console.ReadKey();
-
-
-            //Metadata metadata2 = md.GetMetadata("C:\\BackupTesting");
-
-            //foreach (var item in metadata2.Items)
-            //{
-            //    Console.WriteLine(item.ItemPath);
-            //}
-
-            //Console.ReadKey(); 
-
-
             BackupService b = new BackupService();
 
             Config config = new Config(668, "Testing10", _BackupType.DI);
@@ -46,25 +19,7 @@ namespace DaemonOfPain
             config.Destinations.Add(new Destination(@"C:\BackupDest", DestType.DRIVE));
             config.Destinations.Add(new Destination(@"C:\BackupDest2\Backups", DestType.DRIVE));
 
-
-            //Config config = new Config(663, "Testing3", _BackupType.DI);
-            //config.Retention[0] = 3;
-            //config.Retention[1] = 4;
-            //config.Sources.Add(@"C:\Users\František\Desktop\BackupTesting\SourceDir");
-            //config.Destinations.Add(new Destination(@"C:\Users\František\Desktop\BackupTesting\DestDir", DestType.DRIVE));
-
-            //int count = 15;
-            //for (int i = 0; i < count; i++)
-            //{
-            //    b.BackupSetup(config);
-            //    Thread.Sleep(2000);
-            //    Console.WriteLine(i);
-            //}
-
-
             b.BackupSetup(config);
-
-
         }
     }
 }
