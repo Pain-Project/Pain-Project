1f653ae4f5aa8d48f85ad8a09d1c0ff43d96369d
melovin <70209304+melovin@users.noreply.github.com>
2022-05-19 00:08:17 +0200

    angular-email-settings-progres

--- Web/Pain-Web/src/app/components/dialogs/settings-dialog/settings.component.ts
+++ Web/Pain-Web/src/app/components/dialogs/settings-dialog/settings.component.ts
@@ -3,7 +3,9 @@ import {DOCUMENT} from "@angular/common";
 import {LoginService, loginUser} from "../../../services/login.service";
 import {UsersService} from "../../../services/users.service";
 import {SessionsService} from "../../../services/sessions.service";
-
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { EmailService } from 'src/app/services/email.service';
+import { EmailSettingsModel } from 'src/app/models/emailSettings.model';
 
 @Component({
   selector: 'Dialog_settings',
@@ -18,13 +20,36 @@ export class SettingsComponent implements OnInit {
   selectedComp = 'one';
   selectedFreq = 'one';
 
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  public form: FormGroup;
+
+  public emailSettings: EmailSettingsModel;
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private rendered: Renderer2,
     private loginService: LoginService,
     private userService: UsersService,
     private sessionsService: SessionsService,
+    private emailService: EmailService,
+    private fb:FormBuilder,
   ) {
+    this.emailService.GetEmailSettings().subscribe(x => {
+      console.log(x)
+      this.emailSettings = x;
+    })
+    this.form = this.fb.group({
+      port:[this.emailSettings.Port,Validators.required],
+      smtp:[this.emailSettings.SMTP, Validators.required],
+      freq:[this.emailSettings.Freq,Validators.required],
+      sender:[this.emailSettings.Sender, Validators.required],
+
+      // port:[0,Validators.required],
+      // smtp:['', Validators.required],
+      // freq:[0,Validators.required],
+      // sender:['', Validators.required],
+    })
   }
 
   ngOnInit(): void {
@@ -54,6 +79,9 @@ export class SettingsComponent implements OnInit {
       this.userService.darkmodeChange(this.darkMode).subscribe(() => this.sessionsService.reLog(this.loginService.GetLogin().Id).subscribe());
       ;
     }
+  }
+  Submit(): void{
+
   }
 }
 
---
+++ Web/Pain-Web/src/app/models/emailSettings.model.ts
@@ -0,0 +1,6 @@
+export class EmailSettingsModel{
+    Port:number = 25;
+    SMTP:string = '';
+    Freq:number = 0;
+    Sender:string = '';
+}
