1b92c41422ba917a96c6a9708c6adda7fdd24e15
TroliksMan <michal.stankoml@gmail.com>
2022-06-08 00:40:14 +0200

    EditConfig Oprava

--- Apps/DaemonOfPain/DaemonOfPain/Application.cs
+++ Apps/DaemonOfPain/DaemonOfPain/Application.cs
@@ -1,6 +1,7 @@
 ﻿using DaemonOfPain.Components;
 using DaemonOfPain.Encryption;
 using DaemonOfPain.Services;
+using Quartz;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,38 +25,48 @@ namespace DaemonOfPain
         }
         public async Task StartApplication()
         {
-            EncryptionKeysManager.NewKeys();
-            EncryptionKeysManager.ServerKey = await APIService.GetServerPublicKey();
-            Settings set = DataService.GetSettings();
-            if (set == null)
+            try
             {
-                string newId = await APIService.LoginToServer();
-                DataService.WriteSettings(new Settings() { Id = newId });
-                IdOfThisClient = newId;
-                if (newId == null)
-                    throw new Exception();//nelze se spojit s databází
-            }
-            else
-            {
-                IdOfThisClient = set.Id;
-            }
-
-            await APIService.GetConfigs();
-
-            TaskManager.UpdateTaskList(DataService.GetAllConfigs());
-            await Timer.SetUp(DataService.GetAllConfigs());
-
-            while (true)
-            {
-                if (DataService.ConfigsWasChanged)
+                EncryptionKeysManager.NewKeys();
+                EncryptionKeysManager.ServerKey = await APIService.GetServerPublicKey();
+                Settings set = DataService.GetSettings();
+                if (set == null)
                 {
-                    DataService.ConfigsWasChanged = false;
-                    await Timer.SetUp(DataService.GetAllConfigs());
+                    string newId = await APIService.LoginToServer();
+                    DataService.WriteSettings(new Settings() { Id = newId });
+                    IdOfThisClient = newId;
+                    if (newId == null)
+                        throw new Exception();//nelze se spojit s databází
                 }
-                    
-                Thread.Sleep(5000);
+                else
+                {
+                    IdOfThisClient = set.Id;
+                }
+
+                await APIService.GetConfigs();
+
+                TaskManager.UpdateTaskList(DataService.GetAllConfigs());
+                await Timer.SetUp(DataService.GetAllConfigs());
+
+                while (true)
+                {
+                    if (DataService.ConfigsWasChanged)
+                    {
+                        DataService.ConfigsWasChanged = false;
+                        await Timer.SetUp(DataService.GetAllConfigs());
+                    }
+
+                    Thread.Sleep(5000);
+                }
             }
-                
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Couldn't connect to API service");
+                Console.WriteLine("Trying again in 5 seconds!");
+                Thread.Sleep(5000);
+                await StartApplication();
+            }
 
         }
     }
